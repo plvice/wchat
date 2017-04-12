2668177d00ae4a5c1b3522b70b2a416448f6fc4c
plvice <prawdziwypiotrek@gmail.com>
2017-04-12 20:11:42 +0200

    draggable test

--- app/components/Login.js
+++ app/components/Login.js
@@ -6,13 +6,77 @@ const UserStore = require('../stores/User');
 const validator = require('validator');
 const ChatComponent = require('./Chat');
 
+var List = React.createClass({
+  getInitialState: function() {
+    return {data: this.props.data};
+  },
+  dragStart: function(e) {
+    this.dragged = e.currentTarget;
+    e.dataTransfer.effectAllowed = 'move';
+    // Firefox requires dataTransfer data to be set
+    e.dataTransfer.setData("text/html", e.currentTarget);
+  },
+  dragEnd: function(e) {
+
+    this.dragged.style.display = "block";
+    this.dragged.parentNode.removeChild(placeholder);
+    // Update data
+    var data = this.state.data;
+    var from = Number(this.dragged.dataset.id);
+    var to = Number(this.over.dataset.id);
+    if(from < to) to--;
+    if(this.nodePlacement == "after") to++;
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    this.setState({data: data});
+  },
+  dragOver: function(e) {
+    e.preventDefault();
+    this.dragged.style.display = "none";
+    if(e.target.className == "placeholder") return;
+    this.over = e.target;
+    // Inside the dragOver method
+    var relY = e.clientY - this.over.offsetTop;
+    var height = this.over.offsetHeight / 2;
+    var parent = e.target.parentNode;
+    
+    if(relY > height) {
+      this.nodePlacement = "after";
+      parent.insertBefore(placeholder, e.target.nextElementSibling);
+    }
+    else if(relY < height) {
+      this.nodePlacement = "before"
+      parent.insertBefore(placeholder, e.target);
+    }
+  },
+  render: function() {
+    return <ul onDragOver={this.dragOver}>
+    	{this.state.data.map(function(item, i) {
+      	return (
+        	<li
+		        data-id={i}
+            key={i}
+            draggable="true"
+            onDragEnd={this.dragEnd}
+            onDragStart={this.dragStart}
+          >
+       			{item}
+          </li>
+        )
+   	 	}, this)}
+    </ul>
+  }
+});
+
+var colors = ["Red","Green","Blue","Yellow","Black","White","Orange"];
+var placeholder = document.createElement("li");
+placeholder.className = "placeholder";
+
 var Login = React.createClass({
     getInitialState: function () {
         var state = UserStore.getUser();
         state.nick = 'Piotr';
         state.email = 'prawdziwypiotrek@outlook.com';
         state.waitingForLogin = false;
-
         // UserAction.restoreUserData(state);
 
         return state;
@@ -65,7 +129,11 @@ var Login = React.createClass({
             });
 
             setTimeout(function () {
-                that.fetchChatData();
+                // that.fetchChatData();
+                that.setState({
+                    waitingForLogin: false
+                });
+                that.renderChatComponent();
                 UserAction.login(that.state);
             }, 800);
         }
@@ -189,6 +257,8 @@ var Login = React.createClass({
         return (
             <div className="wrap">
                 <div className="login">
+                    <List data={colors} />
+
                     <form className="login__form" onSubmit={this.validateForm} noValidate>
                         <div className="avatar">
                             <div className="avatar__img">
--- dist/app-bundle.js
+++ dist/app-bundle.js
@@ -21596,6 +21596,74 @@
 	var validator = __webpack_require__(187);
 	var ChatComponent = __webpack_require__(251);
 	
+	var List = React.createClass({
+	    displayName: 'List',
+	
+	    getInitialState: function getInitialState() {
+	        return { data: this.props.data };
+	    },
+	    dragStart: function dragStart(e) {
+	        this.dragged = e.currentTarget;
+	        e.dataTransfer.effectAllowed = 'move';
+	        // Firefox requires dataTransfer data to be set
+	        e.dataTransfer.setData("text/html", e.currentTarget);
+	    },
+	    dragEnd: function dragEnd(e) {
+	
+	        this.dragged.style.display = "block";
+	        this.dragged.parentNode.removeChild(placeholder);
+	        // Update data
+	        var data = this.state.data;
+	        var from = Number(this.dragged.dataset.id);
+	        var to = Number(this.over.dataset.id);
+	        if (from < to) to--;
+	        if (this.nodePlacement == "after") to++;
+	        data.splice(to, 0, data.splice(from, 1)[0]);
+	        this.setState({ data: data });
+	    },
+	    dragOver: function dragOver(e) {
+	        e.preventDefault();
+	        this.dragged.style.display = "none";
+	        if (e.target.className == "placeholder") return;
+	        this.over = e.target;
+	        // Inside the dragOver method
+	        var relY = e.clientY - this.over.offsetTop;
+	        var height = this.over.offsetHeight / 2;
+	        var parent = e.target.parentNode;
+	
+	        if (relY > height) {
+	            this.nodePlacement = "after";
+	            parent.insertBefore(placeholder, e.target.nextElementSibling);
+	        } else if (relY < height) {
+	            this.nodePlacement = "before";
+	            parent.insertBefore(placeholder, e.target);
+	        }
+	    },
+	    render: function render() {
+	        return React.createElement(
+	            'ul',
+	            { onDragOver: this.dragOver },
+	            this.state.data.map(function (item, i) {
+	                return React.createElement(
+	                    'li',
+	                    {
+	                        'data-id': i,
+	                        key: i,
+	                        draggable: 'true',
+	                        onDragEnd: this.dragEnd,
+	                        onDragStart: this.dragStart
+	                    },
+	                    item
+	                );
+	            }, this)
+	        );
+	    }
+	});
+	
+	var colors = ["Red", "Green", "Blue", "Yellow", "Black", "White", "Orange"];
+	var placeholder = document.createElement("li");
+	placeholder.className = "placeholder";
+	
 	var Login = React.createClass({
 	    displayName: 'Login',
 	
@@ -21604,7 +21672,6 @@
 	        state.nick = 'Piotr';
 	        state.email = 'prawdziwypiotrek@outlook.com';
 	        state.waitingForLogin = false;
-	
 	        // UserAction.restoreUserData(state);
 	
 	        return state;
@@ -21660,7 +21727,11 @@
 	            });
 	
 	            setTimeout(function () {
-	                that.fetchChatData();
+	                // that.fetchChatData();
+	                that.setState({
+	                    waitingForLogin: false
+	                });
+	                that.renderChatComponent();
 	                UserAction.login(that.state);
 	            }, 800);
 	        }
@@ -21786,6 +21857,7 @@
 	            React.createElement(
 	                'div',
 	                { className: 'login' },
+	                React.createElement(List, { data: colors }),
 	                React.createElement(
 	                    'form',
 	                    { className: 'login__form', onSubmit: this.validateForm, noValidate: true },
